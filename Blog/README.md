7079a3def20f70b61c96124d65a59926bf4f4c5b
vassilyev <vassilyev@toowto.kz>
2021-10-20 12:28:45 +0600

    first commit

---
+++ Blog/README.md
@@ -0,0 +1,1 @@
+"# blog"  git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/michaelvassilyevmvc/blog.git git push -u origin main
---
+++ Blog/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
